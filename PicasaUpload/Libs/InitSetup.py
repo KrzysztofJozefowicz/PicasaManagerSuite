146f1d6ef9c886f6030b5dedb3eede0f4e289271
Kristof <kjozef@gmail.com>
2015-09-18 10:19:21 +0200

    Update InitSetup.py

--- PicasaUpload/Libs/InitSetup.py
+++ PicasaUpload/Libs/InitSetup.py
@@ -1,5 +1,5 @@
 '''
-Created on 25-03-2013
+Changed on 18-93-2015
 
 @author: j00zef.git@gmail.com
 '''
@@ -12,8 +12,7 @@ def SetFlags(Settings,Arguments):
    Flags={}
    
    Flags={'Proxy':None,
-       'Username':None,
-       'Password': None,
+       'ClientSecret':None,
        'AlbumListToUpload':None,
        'TitleFromAlbumInfo':True,
        'SummaryFromAlbumInfo':True,
@@ -38,10 +37,13 @@ def SetFlags(Settings,Arguments):
             if i==j and Arguments[i]!=None:
                 Flags[i]=Arguments[i]
     
-    
+   if Flags['ClientSecret']==None or os.path.isfile(Flags['ClientSecret'])==False :
+       print "UserSecret JSON file is not set or file does not exists under path.  \nIf you don't have json file from Google, visit Google Developers Console https://console.developers.google.com"
+       exit(-1)
 
-    
-
+   if Flags['AlbumListToUpload']==None or os.path.exists(Flags['AlbumListToUpload'])==False:
+       print "Flag AlbumListToUpload is not set or path does not exists. Check the input parameters or config.txt. "
+       exit(-1)
    return (Flags)
 
 
@@ -74,19 +76,15 @@ def ArgumentParsing(parser):
 
    
     
-    parser.add_argument("-usr", "--Username"                ,dest="Username"                , help="Sets google username to authenticate in gdata service." ) 
-    parser.add_argument("-psw", "--Password"                ,dest="Password"                , help="Sets google user's password to authenticate in gdata service." )
-    parser.add_argument("-px",  "--Proxy"                   ,dest="Proxy"                   , help="Sets proxy server, format IP:PORT " )       
-    
+    parser.add_argument("-usr", "--UserSecret"                ,dest="UserSecret"                , help="Path to client_secrets JSON file donwloaded from Google Developers Console: https://console.developers.google.com" )
+    parser.add_argument("-px",  "--Proxy"                   ,dest="Proxy"                   , help="Sets proxy server, format IP:PORT " )
     parser.add_argument("-in",  "--Input"                   ,dest="AlbumListToUpload"       , help="Album directory,directory with albums or file list with paths to albums to upload " )
     parser.add_argument("-at",  "--TitleFromAlbumInfo"      ,dest="TitleFromAlbumInfo"      , help="Use album title from album-info file instead of directory name. " )
     parser.add_argument("-as",  "--SummaryFromAlbumInfo"    ,dest="SummaryFromAlbumInfo"    , help="Update album summary from album-info file." )
     parser.add_argument("-ap",  "--PublishedFromAlbumInfo"  ,dest="PublishedFromAlbumInfo"  , help="Update album publish date from album-info file or leave it as current date." )
     parser.add_argument("-al",  "--LocationFromAlbumInfo"   ,dest="LocationFromAlbumInfo"   , help="Update album location from album-info file." )    
-    parser.add_argument("-aa",  "--AccessFromAlbumInfo"     ,dest="AccessFromAlbumInfo"     , help="Update album access type from album-info file or leave it as default (public)." )   
-
-    parser.add_argument("-ac",  "--AlbumCoverUpload"        ,dest="AlbumCoverUpload"        , help="Upload album cover file as photo to ablum." )     
-    
+    parser.add_argument("-aa",  "--AccessFromAlbumInfo"     ,dest="AccessFromAlbumInfo"     , help="Update album access type from album-info file or leave it as default (public)." )
+    parser.add_argument("-ac",  "--AlbumCoverUpload"        ,dest="AlbumCoverUpload"        , help="Upload album cover file as photo to ablum." )
     parser.add_argument("-ft",  "--SummaryFromFile"           ,dest="SummaryFromFile"           , help="Update file title(summary text) from <filename>-title.txt file.If not set, exif entry will be used " )      
     parser.add_argument("-fc",  "--CommentsFromFile"        ,dest="CommentsFromFile"        , help="Update file comments from <filename>-comments.txt file." )     
     parser.add_argument("-fg",  "--GpsFromFile"             ,dest="GpsFromFile"             , help="Update file gps data from <filename>-gps.txt file.If not set, exif entry will be used " )  
